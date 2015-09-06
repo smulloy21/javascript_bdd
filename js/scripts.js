9c0f3d0c2a736fca06a6ad463faf213f93386161
Shannon Mulloy <smulloy21@gmail.com>
2015-09-05 21:25:21 -0700

    work like video lesson

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    $(".not").text("");
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+    if (!result) {
+      $(".not").text("not");
+    }
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
